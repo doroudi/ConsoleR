3289b9b11430ff6a47aadf05a73bcc742caad476
Saeid Doroudi <doroudi@outlook.com>
2024-11-01 11:49:43 +0330

    :tada: start project

---
+++ tests/ConsoleR.TestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleR;
+using ConsoleR.AsciiArt;
+const string message = "Hello, World!";
+
+WriteLine.Print(message);
+WriteLine.Error(message);
+WriteLine.Warning(message);
+WriteLine.Success(message);
+WriteLine.Info(message);
+WriteLine.PrintAsciiArt(message, MessageType.Warning);
+Write.Success(AsciiArt.ToAsciiArt("ConsoleR"));
+Write.Print(AsciiArt.ToAsciiArt("ConsoleR"));
+Write.Error(AsciiArt.ToAsciiArt("ConsoleR"));
