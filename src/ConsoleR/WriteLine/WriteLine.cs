24e91bcb2738a50ee95216aba3f76821291d934b
Saeid Doroudi <doroudi@outlook.com>
2024-11-24 19:01:06 +0330

    Build & Publish Package Fixes #9

--- src/ConsoleR/WriteLine/WriteLine.cs
+++ src/ConsoleR/WriteLine/WriteLine.cs
@@ -9,30 +9,53 @@ public static partial class Consoler
     public static string? ReadLine() {
         return Console.ReadLine();
     }
-    
+
+    public static string? ReadLine(string? prompt) {
+        Console.WriteLine(prompt);
+        return Console.ReadLine();
+    }
+
+    public static string? Read(string? prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+
     public static void WriteLine(string message, ConsoleColor? color = null) 
     {
         DoWriteLine(message,color);
     }
 
 
-    public static void Error(string message)
+    public static void Error(string message, bool showIcon = true)
     {
+        if (showIcon) {
+            message = "❌ " + message;
+        }
         DoWriteLine(message, ConsoleColor.Red);
     }
 
-    public static void Success(string message)
+    public static void Success(string message, bool showIcon = true)
     {
+        if (showIcon) {
+            message = "✅ " + message;
+        }
         DoWriteLine(message, ConsoleColor.Green);
     }
 
-    public static void Info(string message)
+    public static void Info(string message, bool showIcon = true)
     {
+        if (showIcon) {
+            message = "ℹ️ " + message;
+        }
         DoWriteLine(message, ConsoleColor.Blue);
     }
 
-    public static void Warning(string message)
+    public static void Warning(string message, bool showIcon = true)
     {
+        if (showIcon) {
+            message = "⚠️ " + message;
+        }
         DoWriteLine(message, ConsoleColor.Yellow);
     }
 
