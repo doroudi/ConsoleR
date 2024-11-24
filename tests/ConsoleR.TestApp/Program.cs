9e44f613d482f7b04753069d20e1067e753a3b0c
Saeid Doroudi <doroudi@outlook.com>
2024-11-24 19:01:06 +0330

    Build & Publish Package Fixes #9

--- tests/ConsoleR.TestApp/Program.cs
+++ tests/ConsoleR.TestApp/Program.cs
@@ -1,30 +1,29 @@
-﻿using ConsoleR;
-const string message = "Hello, World!";
+﻿using static ConsoleR.Consoler;
+const string message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+AsciiArt("HELLO", ConsoleColor.Yellow);
 
 
-Consoler.Banner("This is ConsoleR, Build Awesome Console Apps with .Net", ConsoleColor.DarkGreen);
-Consoler.ReadKey();
 
-
-Consoler.WriteLine(message, ConsoleColor.Cyan);
-Consoler.Error("Error Occurred in app. please restart");
-Consoler.Success("Task Done successfully");
-Consoler.Info("Press any key to go next demo:");
+WriteLine(message, ConsoleColor.Cyan);
+Error("Error Occurred in app. please restart");
+Success("Task Done successfully");
+Info("Press any key to go next demo:");
 Console.ReadKey();
 string[] frontEndFrameworks = ["Blazor", "Angular", "Vue", "React", "JS"];
-var selectedItem = Consoler.Menu("Please Select One beloved frontend framework", frontEndFrameworks).Select();
-Consoler.Success("Your choice is: \n\n");
-Consoler.AsciiArt(frontEndFrameworks[selectedItem], GetFrameworkColor(frontEndFrameworks[selectedItem]));
+var selectedItem = Menu("Please Select One beloved frontend framework", frontEndFrameworks).Select();
+Success("Your choice is: \n\n");
+AsciiArt(frontEndFrameworks[selectedItem], GetFrameworkColor(frontEndFrameworks[selectedItem]));
 
-Consoler.Info("Ready for next demo? press any key");
-Consoler.ReadKey();
+Info("Ready for next demo? press any key");
+ReadKey();
 
 
 string[] plugins = ["Typescript", "Linter", "Nuxt", "Vite"];
-var selectedItems = Consoler.Checkbox("Select feature that you want to install:", plugins).Select();
+var selectedItems = Checkbox("Select feature that you want to install:", plugins).Select();
 for (int i = 0; i < selectedItems.Length; i++) {
     var plugin = selectedItems[i];
-    Consoler.WriteLine(plugin.Option, (ConsoleColor)i);
+    WriteLine(plugin.Option, (ConsoleColor)i);
 }
 
 
