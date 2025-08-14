b22c44dfbca5c05555f550b68b932e5e4351b3d3
Saeid Doroudi <doroudi@outlook.com>
2025-08-14 10:01:13 +0330

    Fix AsciiArt

--- src/ConsoleR/AsciiArt/AsciiArt.cs
+++ src/ConsoleR/AsciiArt/AsciiArt.cs
@@ -4,7 +4,8 @@ namespace ConsoleR;
 
 public static partial class Console
 {
-    public static void AsciiArt(string message, ConsoleColor? color = null) {
-        WriteLine(AsciiChars.GetAsciiArt(message), color);
+    public static void AsciiArt(string message, ConsoleColor? color = null)
+    {
+        WriteLine(AsciiChars.GetAsciiArt2(message), color);
     }
 }
